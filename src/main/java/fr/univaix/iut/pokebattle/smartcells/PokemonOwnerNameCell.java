d35eaab612b59d8c8fe7f7030578277c59f4fc7b
@albang_ <angel.albaladejo@laposte.net>
2013-03-13 16:10:43 +0100

    magicarpe connait VRAIMENT qui est le boss

---
+++ src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonOwnerNameCell.java
@@ -0,0 +1,22 @@
+package fr.univaix.iut.pokebattle.smartcells;
+
+import fr.univaix.iut.pokebattle.SmartCell;
+import fr.univaix.iut.pokebattle.Tweet;
+
+/**
+	 * Reply to all.
+	 */
+public class PokemonOwnerNameCell implements SmartCell {
+
+	public String Owner = null;
+	
+    public String ask(Tweet question) {
+    	if (question.getText().contains("owner?")) 
+    		if (Owner != null)
+    			return "@" + question.getScreenName() + " my owner is @" + Owner;
+    		else
+    			return "@" + question.getScreenName() + " I have no owner";
+    	return null;
+    }
+    
+}
