90a7e52c6ac2d95d8b88faeac480c32820207c37
Alexandre Biny <alexandre.biny@gmail.com>
2013-03-20 15:08:16 +0100

    Fix : F9 ne fonctionnait pas correctement - F7 vérification de l'owner par .equals() au lieu de ==

--- src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonAttackCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcells/PokemonAttackCell.java
@@ -1,6 +1,5 @@
 package fr.univaix.iut.pokebattle.smartcells;
 
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
@@ -12,44 +11,34 @@ public class PokemonAttackCell implements SmartCell {
 		String skill;
 		String target;
 		String toname;
-		User targetowner;
-		Twitter twitter = TwitterFactory.getSingleton();
 		
 		
-	    public String ask(PokeBot bot, Tweet question) {			
+	    public String ask(PokeBot bot, Tweet question) {
+	    	
 	    	if (question.getText().contains("#attack")) {
-	    		if(bot.Owner == question.getScreenName()) {
+
+	    		if(question.getScreenName().equals(bot.Owner)) {
+	    			
 		    		String[] mots = question.getText().split(" ");
 		    		for (int i = 0; i < mots.length; ++i) {
 						if (mots[i].equals("#attack")){
 							skill = mots[i+1];
-							target = mots[i+2];					
+							target = mots[i+2];
+							toname = mots[i+4];
 
 						}		
 					}
 		    		
-		    		// Get Enemy Owner Name
-		    		try {
-						targetowner = twitter.showUser(target);
-						String TabS[] = targetowner.getDescription().split(" ");
-						
-						int i = 0;
-						while (!TabS[i].equals("Owner:"))
-							++i;
-						toname = "@"+TabS[++i].substring(1)+" ";
-					} 
-		    		catch (TwitterException e) {
-						// TODO Auto-generated catch block
-						toname = "";
-					}
-		    		
-		    		
-		    		String commande = target+" #attack "+skill+"! /cc "+toname+"@"+question.getScreenName();
+		    		String commande = target+" #attack "+skill+"! /cc "+toname+" @"+question.getScreenName();
 		    		return commande;
 		    	}
+	    		
 	    		else if (bot.Owner == null)
 	    			return "@" + question.getScreenName() + " I have no owner";
-	    		return "@" + question.getScreenName() + " my owner is @" + bot.Owner; 
+	    		
+	    		else
+	    			return "@" + question.getScreenName() + " my owner is @" + bot.Owner;
+
 	    	}	
 	    	return null;
 	    }
