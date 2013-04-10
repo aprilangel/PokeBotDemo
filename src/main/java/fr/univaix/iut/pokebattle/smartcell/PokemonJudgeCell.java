b766d6c896369d5cde6bf0ac37c4818ff1d729ac
Alexandre Biny <alexandre.biny@gmail.com>
2013-04-10 16:14:25 +0200

    JudgeCell : More Tests

--- src/main/java/fr/univaix/iut/pokebattle/smartcell/PokemonJudgeCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcell/PokemonJudgeCell.java
@@ -31,7 +31,8 @@ public class PokemonJudgeCell implements SmartCell {
 		        	}
 		        	return " ";
 		    	}
-	    		// Autres messages que le Juge peux dire
+	    		
+		    	// Autres messages que le Juge peux dire
 	    		try {
 	    			/*
 	    			 * TODO
@@ -42,6 +43,12 @@ public class PokemonJudgeCell implements SmartCell {
 	    			
 	    			// Extraction des différents mots
 		    		String[] mots = question.getText().split(" ");
+		    		
+		    		// Phrase trop courte
+		    		if (mots.length < 3)
+		    		{
+		    			return null;
+		    		}
 
 		    		int damage = 0;
 		    		for (int i = 0; i < mots.length - 1 ; ++i )
@@ -51,8 +58,7 @@ public class PokemonJudgeCell implements SmartCell {
 		    				damage = Integer.parseInt(mots[i].substring(0,mots[i].length()-2));
 		    			}
 					}
-					
-					
+		    		
 					
 					// Est-ce que notre vaillant pokémon va t'il succomber a ses blessures ?
 					if (bot.getPv() + damage <= 0) 
